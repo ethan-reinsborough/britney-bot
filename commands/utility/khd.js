83f0da2c89b65403ac0170f0171d7aef244d5f85
Ethan Reinsborough <ethan.reinsborough@gmail.com>
2021-11-20 17:30:48 -0400

    test

--- commands/utility/khd.js
+++ commands/utility/khd.js
@@ -3,7 +3,7 @@ module.exports = {
     description: "Karuta Helper Dictionary: Shows all saved characters.",
     async execute(message) {
         for(i = 0; i < global.characters.length; i++){
-            message.channel.send("\n" + global.characters[i] + ":" + global.characters[i][0]);
+            message.channel.send("\n" + global.characters[i][0] + ":" + global.characters[0][i]);
         }
 
         message.channel.send(table(global.characters));
