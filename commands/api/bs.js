f20304d5203587e4a6ef99987c98eb803a959c86
Ethan Reinsborough <ethan.reinsborough@gmail.com>
2021-11-20 18:03:54 -0400

    yes

--- commands/api/bs.js
+++ commands/api/bs.js
@@ -12,10 +12,17 @@ module.exports = {
             ['MALEVOLENT'],
             ['BIG -- BOOBS; AREOLAS.'],
             ['SYNOPSIS'],
-            ['I SCHLAMMED MY PENITH IN THE CAR DOOR!']
+            ['I SCHLAMMED MY PENITH IN THE CAR DOOR!'],
+            ["HE'S A CLEVER ONE!"],
+            ["MISTY, MISTY! I'M MISTYYY"],
+            ["We got a number one Victory Royale. Yeah, Fortnite, we 'bout to get down (get down)."],
+            ["I'm gonna say a bad word"],
+            ["I'm gonna get racist"],
+            ["meow"],
+            ["breedable."]
         ];
         let wordnum = Math.floor(Math.random() * words.length);
-      message.channel.send(words[wordnum]);
+        message.channel.send(words[wordnum]);
     },
   };
   
