56775b2afec75ebb21d85c2032a6b1a6a8dc40cd
Ethan Reinsborough <ethan.reinsborough@gmail.com>
2021-11-20 21:11:13 -0400

    Fixes

--- commands/api/genshin.js
+++ commands/api/genshin.js
@@ -16,7 +16,7 @@ const fetch = require("node-fetch");
               }
               var randomColor = Math.floor(Math.random()*16777215).toString(16);
               const embed = new MessageEmbed()
-                .setAuthor(`Jojo (Top) | Page ${global.page2} | Post ${global.counter2}`)
+                .setAuthor(`Genshin NSFW (Top) | Page ${global.page2} | Post ${global.counter2}`)
                 .setColor(randomColor)
                 .setImage(r["data"]["children"][global.counter2]["data"]["url"])
                 .setDescription(r["data"]["children"][global.counter2]["data"]["url"]);
