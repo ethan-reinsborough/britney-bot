83cc2a0527df7344459b9eaa28fb82dab53b3756
Ethan Reinsborough <ethan.reinsborough@gmail.com>
2022-01-26 01:05:55 -0400

    Create cl.js

---
+++ commands/utility/cl.js
@@ -0,0 +1,18 @@
+module.exports = {
+    name: "cl",
+    description: "Cool link! Sends a cool Britney-certified link.",
+    async execute(message) {
+        let words = [
+            ['https://www.youtube.com/watch?v=tr8N4lnD3q8'],
+            ['https://www.youtube.com/watch?v=6F5azNTnaOI'],
+            ['https://www.youtube.com/watch?v=k6Yu2SwmU2s'],
+            ['https://www.youtube.com/watch?v=7219-Xa-gfs'],
+            ['https://www.youtube.com/watch?v=9ywnLQywz74'],
+            ['https://youtu.be/n4hm66oOlNI?t=9'],
+            ['https://www.youtube.com/watch?v=kVTAIFi4MJw']
+        ];
+        let wordnum = Math.floor(Math.random() * words.length);
+        message.channel.send(words[wordnum]);
+    },
+  };
+  
