069541c3084533f3665532ba539b93b252a9bd73
Ethan Reinsborough <ethan.reinsborough@gmail.com>
2022-03-16 00:29:07 -0300

    Update v.js

--- commands/api/v.js
+++ commands/api/v.js
@@ -4,41 +4,32 @@ module.exports = {
     name: "v",
     description: "Animal crossing villager lookup!",
     async execute(message) {
-        /*
         input = message.content.split(" ");
 
         if(input[1] == null){
             message.channel.send("Usage: v <villager name>");
             return;
         }
-        var capitalizedName = input[1].charAt(0).toUpperCase() + input[1].slice(1)
-
+        var normalizedText = input[1].toLowerCase();
+        var capitalizedName = normalizedText.charAt(0).toUpperCase() + normalizedText.slice(1);
+        message.channel.send(capitalizedName);
         const res = await fetch(
             `https://acnhapi.com/v1/villagers`
           ).then((response) => response.json());
-        const keys = Object.keys(res);
-        for(let z = 0; z < keys.length; z++){
-            var matchVillager = res[z]
-            if(res[z]["name"]["name-USen"] == capitalizedName){
 
-            }
-        }  
-        */
-        const res = await fetch(
-            `https://acnhapi.com/v1/villagers`
-          ).then((response) => response.json());
-        message.channel.send("TEST");
         for(const key in res){
-            message.channel.send(`${key} : ${res[key]}`);
+            if(res[key]["name"]["name-USen"] == capitalizedName){
+                var villagerId = res[key]["id"];
+                
+                const embed = new MessageEmbed()
+                .setAuthor(`${res[key]["name"]["name-USen"]}`)
+                .setColor("#0000FF")
+                .setThumbnail(`http://acnhapi.com/v1/icons/villagers/${villagerId}`)
+                 message.channel.send(embed);
+                return;
+            }
         }
-        //
-        /*
-        const embed = new MessageEmbed()
-                .setAuthor(`${title} | Rank: ${rank}`)
-                .setColor(randomColor)
-                .setFooter(`ED 1 #${printNum}`)
-                .setImage(res["top"][`${listNum}`]["image_url"])
-                message.channel.send(embed);*/
+        message.channel.send("Villager not found!");
     },
   };
   
